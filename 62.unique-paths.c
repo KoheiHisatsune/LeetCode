e6218e43e70707de33b5467735cc996de181bcd1
hisatsune <hisatsune@m3l.co.jp>
2021-09-28 10:20:35 +0900

    Solved Unique Path

---
+++ 62.unique-paths.c
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=62 lang=c
+ *
+ * [62] Unique Paths
+ */
+
+// @lc code=start
+
+int calcPaths(int** memo, int x, int y);
+
+int uniquePaths(int m, int n){
+
+  int i;
+  int** memo = malloc(sizeof(int *) * m);
+
+  for (i = 0; i < m; i++){
+    memo[i] = malloc(sizeof(int) * n);
+    memset(memo[i],-1,sizeof(int) * n);
+  }
+
+  return calcPaths(memo,m - 1, n - 1);
+
+}
+
+int calcPaths(int** memo, int x, int y)
+{
+  if (x < 0 || y < 0){
+    return 0;
+  }
+
+  if (memo[x][y] == -1){
+    memo[x][y] = (x == 0 && y == 0)? 1 : calcPaths(memo,x - 1, y) + calcPaths(memo,x, y - 1);
+  }
+
+  return memo[x][y];
+
+}
+// @lc code=end
+
