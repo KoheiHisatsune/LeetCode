256dd63b56e3ea9fbdf3f69a69807d81550de08d
hisatsune <hisatsune@m3l.co.jp>
2021-09-30 19:33:34 +0900

    Solved remove nth node

--- 19.remove-nth-node-from-end-of-list.c
+++ 19.remove-nth-node-from-end-of-list.c
@@ -13,33 +13,59 @@
  * };
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-struct ListNode* removeNthFromEnd(struct ListNode* head, int n){
-
-  struct ListNode* target;
-  struct ListNode* start;
-  struct ListNode* target =
-
-  while (head != NULL){
-
-  }
-
-
-}
-
-struct ListNode* ListPoint(struct ListNode* start, int num)
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n)
 {
+  int count = 0;
   int i;
-  struct ListNode* temp = start;
+  struct ListNode* node = head;
+  struct ListNode* temp;
+  struct ListNode* tail = NULL;
+  struct ListNode* secondNodeFromEnd = NULL;
+  struct ListNode* prevNode;
+  struct ListNode* nextNode;
 
-  for (i = 0; i < num; i++){
-    if (temp->next == NULL){
-      return temp->next;
-    }
-    temp = temp->next;
+  while (node != NULL){
+    count++;
+    secondNodeFromEnd = tail;
+    tail = node;
+    node = node->next;
   }
 
-  return temp;
+  if (count == 1){
+    free(head);
+    return NULL;
+  }
+
+  if (n == count){
+    temp = head;
+    head = head->next;
+    free(temp);
+    return head;
+  }
+  else if (n == 1){
+    secondNodeFromEnd->next = NULL;
+    free(tail);
+    return head;
+  }
+  else{
+    temp = head;
+
+    for (i = 0; i < count - n - 1; i++){
+      temp = temp->next;
+    }
+
+    prevNode = temp;
+    temp = temp->next;
+    nextNode = temp->next;
+    prevNode->next = nextNode;
+    free(temp);
+    return head;
+  }
+
 }
 // @lc code=end
 
